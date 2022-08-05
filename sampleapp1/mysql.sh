ea615ea39db457780f2931420a539a382c90d2ea
blazarovteladoc <97243122+blazarovteladoc@users.noreply.github.com>
2022-08-05 13:55:29 +0300

    Update mysql.sh

--- sampleapp1/mysql.sh
+++ sampleapp1/mysql.sh
@@ -9,5 +9,4 @@ rds_user=`aws --region ${AZ::-1} ssm get-parameters --names ${APPSTACKID}_rdsuse
 rds_pass=`aws --region ${AZ::-1} ssm get-parameters --names ${APPSTACKID}_rdspass --query 'Parameters[0].Value' --output text --with-decryption`
 
 rds_endpoint=`aws --region ${AZ::-1} rds describe-db-instances --db-instance-identifier $rds_instance_id | jq -r ".DBInstances[].Endpoint.Address"`
-mysql -h $rds_endpoint -P 3306 -u $rds_user -p$rds_pass movies < $1
-
+mysql -h $rds_endpoint -P 3306 -u $rds_user -p$rds_pass < $1
